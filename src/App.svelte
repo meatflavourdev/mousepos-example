62eca4b4a38c305f2295da76ee0338d8157214c0
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-01-09 03:21:12 -0800

    Changed server address

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
   // All Yjs types must be embedded in a Y.Doc
   const ydoc = new Y.Doc();
 
-  const wsUrl = 'wss://2dfc6998bfab.ngrok.io';
+  const wsUrl = 'wss://143.110.155.245';
   // Connect our Y.Doc to the sync server. Note that you could also use p2p
   // via webrtc (due to Yjs' CRDT convergence algorithm, no server necessary).
   new WebsocketProvider(wsUrl, 'example', ydoc);
